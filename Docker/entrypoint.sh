bf934a5a0d197bbf349fd83e19ed9266dccc203a
AbsharHassan <abshar.hassan7@gmail.com>
2023-02-25 23:53:50 +0500

    entrypoint added, unmounted volumes

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,13 +2,13 @@
 
 echo "entered entrypoint file"
 
-if [ ! -f "vendor/autoload.php" ]; then
-    echo "will install composer dependancies"
-    composer install --no-progress --no-interaction
-else
-    echo "dependacnies already installed, will update"
-    composer update --no-progress --no-interaction     
-fi
+# if [ ! -f "vendor/autoload.php" ]; then
+#     echo "will install composer dependancies"
+#     composer install --no-progress --no-interaction
+# else
+#     echo "dependacnies already installed, will update"
+#     composer update --no-progress --no-interaction     
+# fi
 
 # if [ ! -d "node_modules" ]; then
 #     echo "will install npm stuff"
@@ -17,8 +17,13 @@ fi
 #     echo "no need to install npm stuff, will update"
 #     npm update --no-progress --no-interaction
 # fi
+
+composer install
+
 npm install
 
 npm run prod
+
+echo "all dependencies installed"
 
 php-fpm
